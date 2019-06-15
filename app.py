9a2c7ff5ab4afee5dec5240401d759238243cf19
Tyler Collins <tjscollins@gmail.com>
2019-06-15 08:29:04 -1000

    Tweak startup to get correct redis host depending on environment

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ from redis import Redis, RedisError
 import os
 import socket
 
+is_production_env = os.getenv('PYTHON_ENV') == 'production'
+
 # Connect to Redis
-redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+if is_production_env:
+    redis_host = 'redis'
+else:
+    redis_host = 'localhost'
+redis = Redis(host=redis_host, db=0, socket_connect_timeout=2, socket_timeout=2)
 
 app = Flask(__name__)
 
@@ -21,7 +27,7 @@ def hello():
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
 if __name__ == "__main__":
-    if os.getenv('PYTHON_ENV') == 'production':
+    if is_production_env:
         print(f"Cannot run directly in production")
     else:
         app.run(host='0.0.0.0', port=3000)
